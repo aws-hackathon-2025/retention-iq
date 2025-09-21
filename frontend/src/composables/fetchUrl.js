ebee234aec10c746df509c4a16eccba2bd15019a
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-09-21 22:53:23 +0800

    Update the fetchUrl composable.

--- frontend/src/composables/fetchUrl.js
+++ frontend/src/composables/fetchUrl.js
@@ -1,7 +1,13 @@
-export const fetchUrl = async (url, method, headers, isLoading = null) => {
+export const fetchUrl = async (
+  url,
+  method,
+  headers,
+  isLoading = null,
+  body = null
+) => {
   try {
     if (isLoading) isLoading.value = true;
-    const res = await fetch(url, { method, headers });
+    const res = await fetch(url, { method, headers, body });
     const data = await res.json();
     return data;
   } catch (e) {
